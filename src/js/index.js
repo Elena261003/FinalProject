7f11862ba0af903649835c77dd17528028adaed1
monre036 <samsonova.lena.03@gmail.com>
2024-05-09 18:53:47 +0200

    uu

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+const showmoreElement = document.getElementById('showmore');
+const menuElement = document.getElementById('menu');
+
+function handleMenu() {
+  if (menuElement.classList.contains('show')) {
+    menuElement.classList.remove('show');
+  } else {
+    menuElement.classList.add('show');
+  }
+}
+
+showmoreElement.addEventListener('click', handleMenu);
