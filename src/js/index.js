f4a4ad7205087e7eee75908bd4e2b476fce70720
UMessina <164208007+Elena261003@users.noreply.github.com>
2024-05-23 18:52:26 +0200

    ddddd

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,36 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+/*const actualYear = (year, age) => {
+  console.log(`Naciste en el año ${year - age}`);
+  for (let counter = 1; counter <= age; counter++) {
+    console.log(`En el año ${year - age + counter} cumpliste ${counter} años`);
+    if (year - age + counter === year - age + 1) {
+      console.log(`En el año ${year - age + counter} cumpliste ${counter} año`);
+    } else {
+      console.log(
+        `En el año ${year - age + counter} cumpliste ${counter} años`
+      );
+    }
+  }
+};
+actualYear(2024, 21);
+
+const evenNumbers = (a, b) => {
+  let start;
+  let end;
+
+  if (a < b) {
+    start = a;
+    end = b;
+  } else {
+    start = b;
+    end = a;
+  }
+  for (let counter = start; counter <= end; counter++) {
+    if (counter % 2 === 0) {
+      console.log(counter);
+    }
+  }
+};
+evenNumbers(2, 12);
+*/
